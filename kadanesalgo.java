edb789c434c22c1390c46cc21757bb3b23e73956
Ayush Kumar Singh <ayushxsingh100@outlook.com>
2025-03-24 17:59:37 +0530

    DSA USING ARRAYS ( DAY - 5 )

---
+++ kadanesalgo.java
@@ -0,0 +1,21 @@
+public class kadanesalgo {
+    public static void printmaxsum(int numbers[]){
+      int ms = Integer.MIN_VALUE;
+      int cs = 0;
+       
+      for( int i = 0 ; i<numbers.length ; i++){
+          cs = cs + numbers[i];
+          if(cs<0){
+            cs=0;
+          }
+          ms = Math.max(cs, ms);
+      }
+      System.out.println("the max subarray is " + ms);
+        
+    }
+    public static void main(String[] args) {
+        int [] numbers = { 1,-2,6,-1,3};
+        printmaxsum(numbers);
+        
+    }
+}
