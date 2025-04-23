5486bbce42f95276887118bd2a9bf443bcab685b
Ayush Kumar Singh <ayushxsingh100@outlook.com>
2025-04-23 14:43:48 +0530

    REMOVE DUPLICATES FROM S.ARRAY ( LEETCODE DAY -13)

---
+++ LEETCODE13.java
@@ -0,0 +1,13 @@
+public static int removeD(int [] nums){
+    if (nums == null || nums.length == 0){
+        return 0;
+    }
+    int i =0;
+    for(int j = 1 ; j< nums.length-1 ; j++){
+        if(nums[j] != nums[i]){
+            i++;
+            nums[i] = nums[j];
+        }
+           return i+1;
+    }
+}
